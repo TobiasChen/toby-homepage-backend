6288cf50091c50c8f39fca3262e08df4281e88fd
Tobias Chen <tobias@chen.de>
2023-05-11 20:15:24 +0200

    Fixed Test

--- aws/test/visitorCount.test.js
+++ aws/test/visitorCount.test.js
@@ -12,7 +12,6 @@ jest.mock('aws-sdk', () => {
     });
 
 
-
 test("On Get, return 200 with Value",async ()=> {
     const event = {"httpMethod": "GET"}
     await expect( vis.handler(event)).resolves.toEqual({"body": "{\"visits\":1}", "headers": {"Content-Type": "application/json"}, "statusCode": 200})
@@ -20,7 +19,7 @@ test("On Get, return 200 with Value",async ()=> {
 
 test("On non existing HTTP Method, return 405",async ()=> {
     const event = {"httpMethod": "XYZ"}
-    await expect( vis.handler(event)).resolves.toEqual({"body": "\"Unsupported method \\\"XY2Z\\\"\"", "headers": {"Content-Type": "application/json"}, "statusCode": 405})
+    await expect( vis.handler(event)).resolves.toEqual({"body": "\"Unsupported method \\\"XYZ\\\"\"", "headers": {"Content-Type": "application/json"}, "statusCode": 405})
 })
 
 test("On non supported HTTP Method, return 405",async ()=> {
